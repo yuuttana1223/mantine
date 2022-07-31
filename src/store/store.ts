ed3d7f630d4a77bda1f74dffc5720e4511a7a648
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-07-31 20:59:08 +0900

    🔥 storeからavatar関連を削除

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,15 +5,11 @@ import { devtools } from "zustand/middleware";
 type State = {
   session: Session | null;
   setSession: (payload: Session | null) => void;
-  avatarUrl: string;
-  setAvatarUrl: (payload: string) => void;
 };
 
 export const useStore = create(
   devtools<State>((set) => ({
     session: null,
     setSession: (payload) => set({ session: payload }),
-    avatarUrl: "",
-    setAvatarUrl: (payload) => set({ avatarUrl: payload }),
   }))
 );
